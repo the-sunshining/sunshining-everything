3269b800b4945e32eeed5164c0d347ea48897638
The SunShining <thesunshiningmin@gmail.com>
2020-10-29 14:16:04 -0600

    some changes

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
         $(".Nav_C").click(function(){
             $("#TheSunShiningEverythingSidebar").toggle();
+            $(".header_p").toggle();
         });
     
     
