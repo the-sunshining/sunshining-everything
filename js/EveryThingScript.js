4e730f785985886250582ff72a354c3d2cc0aee1
The SunShining <thesunshiningmin@gmail.com>
2020-10-29 22:23:06 -0600

    slight changes not fineshed on everything script

--- js/EveryThingScript.js
+++ js/EveryThingScript.js
@@ -1,72 +1,61 @@
-function linksWork(){
-document.getElementById("SunHome").addEventListener("click", showHome);
-document.getElementById("SunBible").addEventListener("click", showBible);
-document.getElementById("JesusLife").addEventListener("click", showJesus);
-document.getElementById("toHeaven").addEventListener("click", showHeaven);
-document.getElementById("TeaCup").addEventListener("click", showCup);
-document.getElementById("BookMarket").addEventListener("click", showBookMarket);
-document.getElementById("More").addEventListener("click", showMore);
-};
-function showHome() {
-document.getElementById('TheSunShiningHeadSite').style.display='block';
-document.getElementById('SunBibleSite').style.display='none';
-document.getElementById('LifeOfJesusSite').style.display='none';
-document.getElementById('HowToGetToHeavenSite').style.display='none';
-document.getElementById('MyChristianTeacupSite').style.display='none';
-document.getElementById('MySmallOnlineBookMarketSite').style.display='none';
-document.getElementById('more').style.display='none'
-};
-function showBible(){
-document.getElementById('TheSunShiningHeadSite').style.display='none';
-document.getElementById('SunBibleSite').style.display='block';
-document.getElementById('LifeOfJesusSite').style.display='none';
-document.getElementById('HowToGetToHeavenSite').style.display='none';
-document.getElementById('MyChristianTeacupSite').style.display='none';
-document.getElementById('MySmallOnlineBookMarketSite').style.display='none';
-document.getElementById('more').style.display='none'
-};
-function showJesus(){
-document.getElementById('TheSunShiningHeadSite').style.display='none';
-document.getElementById('SunBibleSite').style.display='none';
-document.getElementById('LifeOfJesusSite').style.display='block';
-document.getElementById('HowToGetToHeavenSite').style.display='none';
-document.getElementById('MyChristianTeacupSite').style.display='none';
-document.getElementById('MySmallOnlineBookMarketSite').style.display='none';
-document.getElementById('more').style.display='none';
-};
-function showHeaven(){
-	document.getElementById('TheSunShiningHeadSite').style.display='none';
-document.getElementById('SunBibleSite').style.display='none';
-document.getElementById('LifeOfJesusSite').style.display='none';
-document.getElementById('HowToGetToHeavenSite').style.display='block';
-document.getElementById('MyChristianTeacupSite').style.display='none';
-document.getElementById('MySmallOnlineBookMarketSite').style.display='none';
-document.getElementById('more').style.display='none';
-};
-function showCup(){
-	document.getElementById('TheSunShiningHeadSite').style.display='none';
-document.getElementById('SunBibleSite').style.display='none';
-document.getElementById('LifeOfJesusSite').style.display='none';
-document.getElementById('HowToGetToHeavenSite').style.display='none';
-document.getElementById('MyChristianTeacupSite').style.display='block';
-document.getElementById('MySmallOnlineBookMarketSite').style.display='none';
-document.getElementById('more').style.display='none';
-};
-function showBookMarket(){
-	document.getElementById('TheSunShiningHeadSite').style.display='none';
-document.getElementById('SunBibleSite').style.display='none';
-document.getElementById('LifeOfJesusSite').style.display='none';
-document.getElementById('HowToGetToHeavenSite').style.display='none';
-document.getElementById('MyChristianTeacupSite').style.display='none';
-document.getElementById('MySmallOnlineBookMarketSite').style.display='block';
-document.getElementById('more').style.display='none';
-};
-function showMore(){
-	document.getElementById('TheSunShiningHeadSite').style.display='none';
-document.getElementById('SunBibleSite').style.display='none';
-document.getElementById('LifeOfJesusSite').style.display='none';
-document.getElementById('HowToGetToHeavenSite').style.display='none';
-document.getElementById('MyChristianTeacupSite').style.display='none';
-document.getElementById('MySmallOnlineBookMarketSite').style.display='none';
-document.getElementById('more').style.display='block';
-}
+$(document).ready(function(){
+
+	$("#SunHome").click(function(){
+		$("#TheSunShiningHeadSite").css("display", "block");
+		$("#SunBibleSite").css("display", "none");
+		$("#LifeOfJesusSite").css("display", "none");
+		$("#HowToGetToHeavenSite").css("display", "none");
+		$("#MyChristianTeacupSite").css("display", "none");
+		$("#more").css("display", "none");
+	});
+
+	$("#SunBible").click(function(){
+		$("#TheSunShiningHeadSite").css("display", "none");
+		$("#SunBibleSite").css("display", "block");
+		$("#LifeOfJesusSite").css("display", "none");
+		$("#HowToGetToHeavenSite").css("display", "none");
+		$("#MyChristianTeacupSite").css("display", "none");
+		$("#more").css("display", "none");
+	});
+
+	$("#JesusLife").click(function(){
+		$("#TheSunShiningHeadSite").css("display", "none");
+		$("#SunBibleSite").css("display", "none");
+		$("#LifeOfJesusSite").css("display", "block");
+		$("#HowToGetToHeavenSite").css("display", "none");
+		$("#MyChristianTeacupSite").css("display", "none");
+		$("#more").css("display", "none");
+	});
+
+	$("#toHeaven").click(function(){
+		$("#TheSunShiningHeadSite").css("display", "block");
+		$("#SunBibleSite").css("display", "none");
+		$("#LifeOfJesusSite").css("display", "none");
+		$("#HowToGetToHeavenSite").css("display", "none");
+		$("#MyChristianTeacupSite").css("display", "none");
+		$("#more").css("display", "none");
+	});
+
+	$("#TeaCup").click(function(){
+		$("#TheSunShiningHeadSite").css("display", "block");
+		$("#SunBibleSite").css("display", "none");
+		$("#LifeOfJesusSite").css("display", "none");
+		$("#HowToGetToHeavenSite").css("display", "none");
+		$("#MyChristianTeacupSite").css("display", "none");
+		$("#more").css("display", "none");
+	});
+
+	$("#More").click(function(){
+		$("#TheSunShiningHeadSite").css("display", "block");
+		$("#SunBibleSite").css("display", "none");
+		$("#LifeOfJesusSite").css("display", "none");
+		$("#HowToGetToHeavenSite").css("display", "none");
+		$("#MyChristianTeacupSite").css("display", "none");
+		$("#more").css("display", "none");
+	});
+
+
+
+
+	
+	});
--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 
         $(".Nav_C").click(function(){
             $("#TheSunShiningEverythingSidebar").toggle();
-            $(".header_p").toggle();
         });
     
     
