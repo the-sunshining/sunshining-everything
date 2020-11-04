f01e8e2ff4f519e3a62fccce2ed2d609a743c55a
The SunShining <thesunshiningmin@gmail.com>
2020-11-03 19:53:56 -0700

    some

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-The SunShining EveryThing
-Quick and easy access to everything by The SunShining
+# The SunShining EveryThing
+## Quick and easy access to everything by The SunShining
 
 Since I am not good at making readme's.
-I will explain somthings.
+I will explain some things.
+I once created a SunShining Shortcut extention.
+I once started on a project called the sunshining everything.
+I decided to combine them.
+### The SunShining is a ministry devoted to sharing godly resources!
---
+++ main.js
@@ -0,0 +1,76 @@
+//I have no idea what most of this does!!! - NCR
+
+const { app, BrowserWindow, Menu, session } = require('electron')
+
+
+// Main SunBible App Activity: 
+app.on('ready', async () => {
+  // We cannot require the screen module until the
+  // app is ready
+  const { screen } = require('electron')
+
+  // Create a window that fills the sceen's available  work area.
+  const primaryDisplay = screen.getPrimaryDisplay()
+  const { width, height } = primaryDisplay.workAreaSize
+
+  const mainWindow = new BrowserWindow({
+    width,
+    height,
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+})
+
+
+
+
+
+
+
+// Everything menu
+const isMac = process.platform === 'darwin'
+const template = [
+  {
+    label: 'SunBible',
+    submenu: [
+    { label: 'Copy Bible Verses', role: 'copy' },
+      { type: 'separator' },
+      {
+        label: 'View SunBible Online',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://the-sunshining.github.io/SunBible/')
+        }
+      },
+      { type: 'separator' },
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  }
+]
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+
+
+// Right Click Menu using - "electron-context-menu": "^2.3.0"
+const contextMenu = require('electron-context-menu');
+contextMenu({
+	menu: (actions, props, browserWindow) => [
+    {
+      label: 'SunBible',
+      submenu: [
+      { label: 'Copy Bible Verses', role: 'copy' },
+        { type: 'separator' }
+      ]
+    }
+		
+	]
+});
+
+
+
+ 
