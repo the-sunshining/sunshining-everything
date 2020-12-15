74b38530bf698f4628a6d69e4578c9b03a867067
TheSun <thesunshiningmin@gmail.com>
2020-12-15 12:04:57 -0600

    nav change

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
 
         $(".Nav_C").click(function(){
             $("#TheSunShiningEverythingSidebar").toggle();
+            $(".Nav_C").toggleClass("change");
         });
     
     
         
         });
+
+
+        // function myFunction(x) {
+        //      x.classList.toggle("change");
+        //   }
